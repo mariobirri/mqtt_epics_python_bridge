1c117509c63b44a147a4b6bc90e41d986ffb953b
mariobirri <mario.birri@psi.ch>
2020-08-17 13:15:54 +0000

    geändert:       epicsDriver.py

--- epicsDriver.py
+++ epicsDriver.py
@@ -5,32 +5,32 @@ class myDriver(Driver):
     def __init__(self):
         super(myDriver, self).__init__()
     def read(self, reason):
-        if reason == 'SEND': global msgFill; value = msgFill;
-	elif reason == 'V1': global msgV1; value = msgV1;
-	elif reason == 'V2': global msgV2; value = msgV2;
-	elif reason == 'V3': global msgV3; value = msgV3;
-        elif reason == 'V4': global msgV4; value = msgV4;
-	elif reason == 'V5': global msgV5; value = msgV5;
-        elif reason == 'V6': global msgV6; value = msgV6;
-        elif reason == 'V7': global msgV7; value = msgV7;
-        elif reason == 'V8': global msgV8; value = msgV8;
-	elif reason == 'DATAC': global msgDataC; value = msgDataC;
-	elif reason == 'DATAV': global msgDataV; value = msgDataV;
-	elif reason == 'DATAP': global msgDataP; value = msgDataP;
-        elif reason == 'PRESSURE1': global pressure1; value = pressure1;
-        elif reason == 'PRESSURE2': global pressure2; value = pressure2;
-        elif reason == 'PRESSURE.SETPOINT': global pressureSP; value = pressureSP;
-        elif reason == 'MIX1': global msgMix1; value = msgMix1;
-        elif reason == 'MIX2': global msgMix2; value = msgMix2;
-        elif reason == 'MIX3': global msgMix3; value = msgMix3;
-        elif reason == 'MIX4': global msgMix4; value = msgMix4;
-        elif reason == 'RECEIPT': global receipt; value = receipt;
-        elif reason == 'G1': global msgGas1; value = msgGas1;
-        elif reason == 'G2': global msgGas2; value = msgGas2;
-	elif reason == 'PG1': global msgPGas1; value = msgPGas1;
-        elif reason == 'PG2': global msgPGas2; value = msgPGas2;
-        elif reason == 'ENERGY': global energy; value = energy;
-        elif reason == 'CORESPONDING': global coresponding; value = coresponding;
+
+	#DATAV
+	if reason == 'V1': global V1; value = V1;
+	elif reason == 'V2': global V2; value = V2;
+	elif reason == 'V3': global V3; value = V3;
+        elif reason == 'V4': global V4; value = V4;
+	elif reason == 'V5': global V5; value = V5;
+        elif reason == 'V6': global V6; value = V6;
+        elif reason == 'V7': global V7; value = V7;
+        elif reason == 'V8': global V8; value = V8;
+
+	#DATAP
+        elif reason == 'PPC': global ppc; value = ppc;
+        elif reason == 'PPV': global ppv; value = ppv;
+
+	#DATAC
+	elif reason == 'IC': global ic; value = ic;
+        elif reason == 'GAS1': global gas1; value = gas1;
+        elif reason == 'GAS2': global gas2; value = gas2;
+        elif reason == 'PGAS1': global pgas1; value = pgas1;
+        elif reason == 'PGAS2': global pgas2; value = pgas2;
+        elif reason == 'CYCLE': global cycle; value = cycle;
+        elif reason == 'ACTCYCLE': global actcycle; value = actcycle;
+        elif reason == 'STATUS': global status; value = status;
+	elif reason == 'SETPOINT': global setpoint; value = setpoint;
+
 	else: value = self.getParam(reason)
 	return value
 
